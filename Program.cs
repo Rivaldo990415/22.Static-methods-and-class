299f2e8096fd2656cc3cf2b3c877febca05e84bc
SBICZA01\A253638 <rivaldo.mkhola@standardbank.co.za>
2023-12-06 22:11:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_methods_and_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math.Sqrt(144));
+
+            UsefulTools.sayHi("evans");
+
+            //
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ UsefulTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_methods_and_class
+{
+    internal class UsefulTools
+    {
+        public static void sayHi(String name)
+        {
+            console.WriteLine("Hi "+name);
+        }
+    }
+}
